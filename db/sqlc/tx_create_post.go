963641b87e0db6fef81146fe9335d1d12504ffa7
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2024-09-29 13:26:14 +0900

    feat: summary登録ロジックの実装

--- db/sqlc/tx_create_post.go
+++ db/sqlc/tx_create_post.go
@@ -33,7 +33,12 @@ func (r *SQLRepository) CreatePostTx(ctx context.Context, param CreatePostTxPara
 	if err != nil {
 		return Post{}, err
 	}
-	defer tx.Rollback(ctx)
+	defer func() {
+		// 正常にコミットされた場合は、ロールバックしない
+		if err != nil {
+			tx.Rollback(ctx)
+		}
+	}()
 	qtx := r.WithTx(tx)
 
 	decimalExpense, err := util.ToDecimal(param.Expense)
@@ -57,11 +62,12 @@ func (r *SQLRepository) CreatePostTx(ctx context.Context, param CreatePostTxPara
 		return Post{}, err
 	}
 
-	err = handleSubCategory(ctx, param.PostID, param.SubCategory1, "1", qtx)
+	//categoryIdをバッファ付きチャネルに入れたら良さそう
+	categoryId1, err := handleSubCategory(ctx, param.PostID, param.SubCategory1, "1", qtx)
 	if err != nil {
 		return Post{}, err
 	}
-	err = handleSubCategory(ctx, param.PostID, param.SubCategory2, "2", qtx)
+	categoryId2, err := handleSubCategory(ctx, param.PostID, param.SubCategory2, "2", qtx)
 	if err != nil {
 		return Post{}, err
 	}
@@ -80,6 +86,21 @@ func (r *SQLRepository) CreatePostTx(ctx context.Context, param CreatePostTxPara
 		return Post{}, err
 	}
 
+	//for-selectが使えそう
+	err = calcSubcategorySummary(ctx, CalcSummaryParam, categoryId1, qtx)
+	if err != nil {
+		return Post{}, err
+	}
+	err = calcSubcategorySummary(ctx, CalcSummaryParam, categoryId2, qtx)
+	if err != nil {
+		return Post{}, err
+	}
+
+	err = calcDailyActivitySummary(ctx, CalcSummaryParam, qtx)
+	if err != nil {
+		return Post{}, err
+	}
+
 	err = tx.Commit(ctx)
 	if err != nil {
 		return Post{}, err
@@ -88,21 +109,21 @@ func (r *SQLRepository) CreatePostTx(ctx context.Context, param CreatePostTxPara
 	return post, nil
 }
 
-func handleSubCategory(ctx context.Context, postID uuid.UUID, categoryName string, categoryNo string, qtx *Queries) error {
+func handleSubCategory(ctx context.Context, postID uuid.UUID, categoryName string, categoryNo string, qtx *Queries) (uuid.UUID, error) {
 	if categoryName == "" {
-		return nil
+		return uuid.Nil, nil
 	}
 
 	categoryId, err := registerSubCategory(ctx, categoryName, qtx)
 	if err != nil {
-		return err
+		return uuid.Nil, err
 	}
 
 	err = registerPostSubCategory(ctx, postID, categoryId, categoryNo, qtx)
 	if err != nil {
-		return err
+		return uuid.Nil, err
 	}
-	return nil
+	return categoryId, nil
 }
 
 func registerSubCategory(ctx context.Context, categoryName string, qtx *Queries) (uuid.UUID, error) {
@@ -161,7 +182,7 @@ func calcExpenseSummary(ctx context.Context, param CalcSummaryParam, qtx *Querie
 	}
 	expenseSummary, err := qtx.GetExpenseSummary(ctx, getExpenseSummaryParam)
 	if err != nil {
-		if !errors.Is(err, pgx.ErrNoRows) {
+		if errors.Is(err, pgx.ErrNoRows) {
 			createExpenseSummaryParam := CreateExpenseSummaryParams{
 				Uid:          param.Uid,
 				Year:         param.year,
@@ -195,6 +216,107 @@ func calcExpenseSummary(ctx context.Context, param CalcSummaryParam, qtx *Querie
 	}
 
 	_, err = qtx.UpdateExpenseSummary(ctx, updateExpenseSummaryParam)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func calcSubcategorySummary(ctx context.Context, param CalcSummaryParam, categoryId uuid.UUID, qtx *Queries) error {
+	getSubcategorySummaryParam := GetSubCategorySummaryParams{
+		Uid:        param.Uid,
+		Year:       param.year,
+		Month:      param.month,
+		CategoryID: categoryId,
+	}
+	subcategorySummary, err := qtx.GetSubCategorySummary(ctx, getSubcategorySummaryParam)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			createSubcategorySummaryParam := CreateSubCategorySummaryParams{
+				Uid:        param.Uid,
+				Year:       param.year,
+				Month:      param.month,
+				CategoryID: categoryId,
+				Amount:     param.Expense,
+			}
+			_, err := qtx.CreateSubCategorySummary(ctx, createSubcategorySummaryParam)
+			if err != nil {
+				return err
+			}
+		}
+		return err
+	}
+
+	if subcategorySummary.Amount.GreaterThan(MaxAmount) {
+		return nil
+	}
+
+	updatedAmount := subcategorySummary.Amount.Add(param.Expense)
+	if updatedAmount.GreaterThan(MaxAmount) {
+		updatedAmount = MaxAmount
+	}
+
+	updateSubcategorySummaryParam := UpdateSubCategorySummaryParams{
+		Amount:     updatedAmount,
+		Uid:        param.Uid,
+		Year:       param.year,
+		Month:      param.month,
+		CategoryID: categoryId,
+	}
+
+	_, err = qtx.UpdateSubCategorySummary(ctx, updateSubcategorySummaryParam)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func calcDailyActivitySummary(ctx context.Context, param CalcSummaryParam, qtx *Queries) error {
+	getDailyActivitySummaryParam := GetDailyActivitySummaryParams{
+		Uid:   param.Uid,
+		Year:  param.year,
+		Month: param.month,
+		Day:   param.day,
+	}
+	dailyActivitySummary, err := qtx.GetDailyActivitySummary(ctx, getDailyActivitySummaryParam)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			createDailyActivitySummaryParam := CreateDailyActivitySummaryParams{
+				Uid:    param.Uid,
+				Year:   param.year,
+				Month:  param.month,
+				Day:    param.day,
+				Number: 1,
+				Amount: param.Expense,
+			}
+			_, err := qtx.CreateDailyActivitySummary(ctx, createDailyActivitySummaryParam)
+			if err != nil {
+				return err
+			}
+		}
+		return err
+	}
+
+	updateDailyActivitySummaryParam := UpdateDailyActivitySummaryParams{
+		Number: dailyActivitySummary.Number + 1,
+		Amount: dailyActivitySummary.Amount,
+		Uid:    param.Uid,
+		Year:   param.year,
+		Month:  param.month,
+		Day:    param.day,
+	}
+
+	if dailyActivitySummary.Amount.GreaterThan(MaxAmount) {
+		qtx.UpdateDailyActivitySummary(ctx, updateDailyActivitySummaryParam)
+	}
+
+	updatedAmount := dailyActivitySummary.Amount.Add(param.Expense)
+	if updatedAmount.GreaterThan(MaxAmount) {
+		updatedAmount = MaxAmount
+	}
+	updateDailyActivitySummaryParam.Amount = updatedAmount
+
+	_, err = qtx.UpdateDailyActivitySummary(ctx, updateDailyActivitySummaryParam)
 	if err != nil {
 		return err
 	}
